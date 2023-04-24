8a90f8a6fef8109e24e7de308d1a5410f55c36cb
RonaldRIvas <00131321@uca.edu.sv>
2023-04-24 13:10:14 -0600

    Laboratorio6

---
+++ LABORATORIO5/app/src/main/java/com/uca/laboratorio5/MovieViewModel.kt
@@ -0,0 +1,34 @@
+package com.uca.laboratorio5
+
+import android.app.Application
+import android.text.Spannable.Factory
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.uca.laboratorio5.data.model.MovieModel
+import com.uca.laboratorio5.repositories.MovieRepository
+import kotlinx.coroutines.channels.Channel
+
+
+class MovieViewModel (private val repository: MovieRepository):ViewModel(){
+
+    fun getMovies() = repository.getMovies()
+
+    fun addMovies(movie: MovieModel) = repository.addMovies(movie)
+
+    companion object{
+        val Factory = viewModelFactory {
+            initializer {
+                val app = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MovieReviewerApplication
+                val repository = MovieRepository(app.movieRepository)
+                MovieViewModel(repository)
+            }
+        }
+
+        private fun MovieRepository(movies: MovieRepository): MovieRepository {
+            TODO("Not yet implemented")
+        }
+    }
+
+}
